d6075a96b480c5fe4b3ab6fe5c73a0ca5eb92743
Egor Danilov <egordanilov970@gmail.com>
2024-06-13 23:29:10 +0300

    Modal initiated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {CommonModule} from "@angular/common";
 import {HeaderComponent} from "../shared/header/header.component";
+import {ModalComponent} from "../shared/modal/modal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
